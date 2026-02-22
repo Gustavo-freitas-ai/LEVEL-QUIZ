eaa893e9d64e1ec6fb85f1c1a36c6394ba095081
Gustavo <gustavobiggg64@gmail.com>
2026-02-21 23:32:32 -0300

    feat: created levels, style levels and modify app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 erros = 0
+level = 0
 
 @app.route("/")
 def home():
@@ -10,10 +11,15 @@ def home():
     erros = 0
     return render_template("index.html")
 
+@app.route("/jlevels")
+def jlevels():
+    return render_template("jlevels.html")
+
 
 @app.route("/level1", methods=["GET", "POST"])
 def level1():
     global erros
+    global level
 
     resultado = ""
 
@@ -22,11 +28,12 @@ def level1():
 
         if resposta == "4":
             erros = 0
+            level += 1
             return redirect("/level2")
 
         else:
             erros += 1
-            resultado = f"Err! 😢 ({erros}/3)"
+            resultado = f"Erro! 😢 ({erros}/3)"
 
         if erros == 3:
             erros = 0
@@ -37,24 +44,29 @@ def level1():
 
 @app.route("/level2", methods=["GET", "POST"])
 def level2():
-    global erros
+    global level
 
-    resultado = ""
+    if level == 1:
+        global erros
 
-    if request.method == "POST":
-        resposta = request.form["resposta"]
+        resultado = ""
 
-        if resposta == "2": 
-            resultado = "You Win! 🥳"
-        else:
-            erros += 1
-            resultado = f"Err! 😢 ({erros}/3)"
+        if request.method == "POST":
+            resposta = request.form["resposta"]
 
-        if erros == 3:
-            erros = 0
-            return redirect("/")
+            if resposta == "2": 
+                resultado = "You Win! 🥳"
+            else:
+                erros += 1
+                resultado = f"Erro! 😢 ({erros}/3)"
 
-    return render_template("level2.html", resultado=resultado)
+            if erros == 3:
+                erros = 0
+                return redirect("/")
+
+        return render_template("level2.html", resultado=resultado)
+    else:
+        return redirect("/")
 
 
 if __name__ == "__main__":
